6eecd4e9c048b941fb087ecd25868e2208b94a0c
Daffaazzi <azzidaffa@gmail.com>
2026-02-24 08:42:07 +0700

    Percobaan 1

--- Percobaan1/MahasiswaDemo08.java
+++ Percobaan1/MahasiswaDemo08.java
@@ -1,25 +1,25 @@
 package Percobaan1;
 
-public class MahasiswaMain08 {
+public class MahasiswaDemo08 {
     public static void main(String[] args){
         Mahasiswa08[] arrayMahasiswa08 = new Mahasiswa08[3];
         arrayMahasiswa08[0] = new Mahasiswa08();
-        arrayMahasiswa08[0].nim = "24410706033";
-        arrayMahasiswa08[0].nama = "Andi";
-        arrayMahasiswa08[0].kelas = "TI-1A";
-        arrayMahasiswa08[0].ipk = (float)3.5f;
+        arrayMahasiswa08[0].nim = "244107060033";
+        arrayMahasiswa08[0].nama = "AGNES TITANIA KINANTI";
+        arrayMahasiswa08[0].kelas = "SIB-1E";
+        arrayMahasiswa08[0].ipk = (float)3.75;
 
         arrayMahasiswa08[1] = new Mahasiswa08();
         arrayMahasiswa08[1].nim = "2341720172";
         arrayMahasiswa08[1].nama = "Budi";
-        arrayMahasiswa08[1].kelas = "TI-1B";
-        arrayMahasiswa08[1].ipk = 3.7f;
+        arrayMahasiswa08[1].kelas = "TI-2A";
+        arrayMahasiswa08[1].ipk = (float)3.36;
 
         arrayMahasiswa08[2] = new Mahasiswa08();
         arrayMahasiswa08[2].nim = "244107023006";
         arrayMahasiswa08[2].nama = "Citra";
-        arrayMahasiswa08[2].kelas = "TI-1C";
-        arrayMahasiswa08[2].ipk = (float)3.9f;
+        arrayMahasiswa08[2].kelas = "TI-2E";
+        arrayMahasiswa08[2].ipk = (float)3.80;
 
         System.out.println(("NIM     : " + arrayMahasiswa08[0].nim));
         System.out.println(("Nama    : " + arrayMahasiswa08[0].nama));
