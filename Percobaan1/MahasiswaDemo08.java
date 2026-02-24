edbceafb25afd2f3e5aae7ad18dd9f17c7a2af83
Daffaazzi <azzidaffa@gmail.com>
2026-02-24 10:44:24 +0700

    percobaan 1

--- Percobaan1/MahasiswaDemo08.java
+++ Percobaan1/MahasiswaDemo08.java
@@ -1,5 +1,7 @@
 package Percobaan1;
 
+import java.util.Scanner;
+
 public class MahasiswaDemo08 {
     public static void main(String[] args){
         Mahasiswa08[] arrayMahasiswa08 = new Mahasiswa08[3];
@@ -11,16 +13,17 @@ public class MahasiswaDemo08 {
 
         arrayMahasiswa08[1] = new Mahasiswa08();
         arrayMahasiswa08[1].nim = "2341720172";
-        arrayMahasiswa08[1].nama = "Budi";
+        arrayMahasiswa08[1].nama = "ACHMAD MAULANA HAMZAH";
         arrayMahasiswa08[1].kelas = "TI-2A";
         arrayMahasiswa08[1].ipk = (float)3.36;
 
         arrayMahasiswa08[2] = new Mahasiswa08();
         arrayMahasiswa08[2].nim = "244107023006";
-        arrayMahasiswa08[2].nama = "Citra";
+        arrayMahasiswa08[2].nama = "DIRHAMAWAN PUTRANTO";
         arrayMahasiswa08[2].kelas = "TI-2E";
         arrayMahasiswa08[2].ipk = (float)3.80;
 
+    
         System.out.println(("NIM     : " + arrayMahasiswa08[0].nim));
         System.out.println(("Nama    : " + arrayMahasiswa08[0].nama));
         System.out.println(("Kelas   : " + arrayMahasiswa08[0].kelas));
@@ -36,5 +39,7 @@ public class MahasiswaDemo08 {
         System.out.println(("Kelas   : " + arrayMahasiswa08[2].kelas));
         System.out.println(("IPK     : " + arrayMahasiswa08[2].ipk));
         System.out.println(("------------------------------"));
+
+         
     }
 }
